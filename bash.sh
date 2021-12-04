8cbed99777195b8bb3cc35542623218044d323ef
aquced <aqucedwgk@gmail.com>
2021-12-04 12:45:03 +0800

    :pencil: update content

--- bash.sh
+++ bash.sh
@@ -1,8 +1,7 @@
 info=$1
-if ["$info" = ""]; then
-  info=":pencil: update content"
+if ["$info" = ""];
+then info=":pencil: update content"
 fi
 git add -A
 git commit -m "$info"
-git push origin hexo
-git push coding hexo
+git push origin main
